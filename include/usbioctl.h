ea82e5182149c832c1c775ad6555110b11dad135
David Grayson <davidegrayson@gmail.com>
2015-09-26 12:15:04 -0700

    Another patch for hclient.

--- include/usbioctl.h
+++ include/usbioctl.h
@@ -576,7 +576,7 @@ typedef union _USB_NODE_CONNECTION_INFORMATION_EX_V2_FLAGS {
     ULONG DeviceIsSuperSpeedCapableOrHigher :1;
     ULONG DeviceIsOperatingAtSuperSpeedPlusOrHigher :1;
     ULONG DeviceIsSuperSpeedPlusCapableOrHigher :1;
-    ULONG ReservedMBZ :30;
+    ULONG ReservedMBZ :28;
   };
 } USB_NODE_CONNECTION_INFORMATION_EX_V2_FLAGS,*PUSB_NODE_CONNECTION_INFORMATION_EX_V2_FLAGS;
 
